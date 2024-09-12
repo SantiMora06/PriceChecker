104130e75ea826937c373ca5d2766ebd28ed03c6
Santi Mera <santigomero@gmail.com>
2024-09-12 11:45:54 +0300

    Refactor technicalIndicators route

--- routes/technicalndicators.routes.js
+++ routes/technicalndicators.routes.js
@@ -1,30 +1,52 @@
 const router = require("express").Router()
-const apiKey = 'UKYI48QGI96XRW9G';
+const apiKey = process.env.apiKey;
 
-router.get(("/:symbol/:interval/:seriestype"), async (req, res) => {
-    const interval = req.params.interval;
-    const symbol = req.params.symbol;
-    const seriesType = req.params.seriestype;
-    const timePeriod = req.params.timePeriod;
+const validSeriesType = ["close", "open", "high", "low"];
+const validInterval = ["1min", "5min", "15min", "30min", "60min", "daily", "weekly", "monthly"]
+const validTechnicalIndicators = {
+    "sma": "SMA", "ema": "EMA", "wma": "WMA", "dema": "DEMA", "tema": "TEMA", "trima": "TRIMA", "kama": "KAMA",
+    "mama": "MAMA", "t3": "T3", "macdext": "MACDEXT", "stochf": "STOCKHF", "stochrsi": "STOCHRSI", "willr": "WILLR",
+    "adx": "ADX", "adxr": "ADXR", "apo": "APO", "ppo": "PPO", "mom": "MOM", "bop": "BOP", "cci": "CCI", "cmo": "CMO",
+    "roc": "ROC", "rocr": "ROCR", "aroon": "AROON", "aroonosc": "AROONOSC", "mfi": "MFI", "trix": "TRIX",
+    "ultosc": "ULTOSC", "dx": "DX", "minus-di": "MINUS_DM", "plus-di": "PLUS_DM", "bbands": "BBANDS",
+    "midpoint": "MIDPOINT", "midprice": "MIDPRICE", "sar": "SAR", "trange": "TRANGE", "atr": "ATR", "natr": "NATR",
+    "ad": "AD", "adosc": "ADOSC", "obv": "OBV", "ht-trendline": "HT_TRENDLINE", "ht-sine": "HT_SINE",
+    "ht-trendmode": "HT_TRENDMODE", "ht-dcperiod": "HT_CDPERIOD", "ht-dcphase": "HT_DCPHASE", "ht-phasor": "HT_PHASOR"
+}
+// We add in here helper functions for validation
+const isValidInterval = (interval) => validInterval.includes(interval)
+const isValidSeriesType = (seriesType) => validSeriesType.includes(seriesType)
+const isValidFunctionType = (functionType) => validTechnicalIndicators.hasOwnProperty(functionType)
 
-    const validInterval = ["1min", "5min", "15min", "30min", "60min", "daily", "weekly", "monthly"]
-    if (!validInterval.includes(interval)) {
-        res.status(500).json({ error: "Input not valid" })
+router.get(("/:functionType/:symbol/:interval/:seriesType"), async (req, res) => {
+
+    const { symbol, interval, seriesType, functionType } = req.params;
+
+    // Validate the interval
+    if (!isValidInterval(interval)) {
+        res.status(400).json({ error: "Input not valid, use 1min, 5min, 15min, 30min, 60min, daily, weekly, monthly" })
     }
-    const validSeriesType = ["close", "open", "high", "low"]
-    if (!validSeriesType.includes(seriesType)) {
-        res.status(500).json({ error: "Input not valid, use close, open, high, low" })
+
+    // Validate the seriesType
+    if (!isValidSeriesType(seriesType)) {
+        res.status(400).json({ error: "Input not valid, use close, open, high, low" })
+    }
+    // Validate the functionType
+    const technicalIndicatorsFunction = validTechnicalIndicators[functionType];
+    if (!technicalIndicatorsFunction) {
+        return res.status(400).json({ error: "Invalid function type" })
     }
 
-    if (isNaN(timePeriod) || timePeriod <= 0) {
-        return res.status(500).json({ error: "Invalid time period, use 1min, 5min, 15min, 30min, 60min, daily, weekly, monthly" })
-    }
-
-    try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=SMA&symbol=${symbol}&interval=${interval}&series_type=${seriesType}&apikey=${apiKey}`)
+    try { // Fetch the data from the Alpha Advantage API
+        const response = await fetch(`https://www.alphavantage.co/query?function=${technicalIndicatorsFunction}&symbol=${symbol}&interval=${interval}&series_type=${seriesType}&apikey=${apiKey}`)
         const data = await response.json()
+        // Check if the data has been sent correctly
+        if (data["Error Message"]) {
+            return res.status(500).json({ error: data["Error Message"] })
+        }
         res.json(data)
     } catch (error) {
+        // Catch general errors
         res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
     }
 })
