9db6c10f3651b2ecf6934cef241d28a4775b3aed
Santi Mera <santigomero@gmail.com>
2024-09-12 11:04:40 +0300

    Refactored commodities route

--- routes/commodities.routes.js
+++ routes/commodities.routes.js
@@ -1,190 +1,45 @@
 const router = require("express").Router()
-const apiKey = 'UKYI48QGI96XRW9G';
+const apiKey = process.env.apiKey;
 
-router.get("/price/crude-oil-wti/:interval", async (req, res) => {
-    const interval = req.params.interval
+const validInterval = ["daily", "weekly", "monthly"];
+const validCommodities = {
+    "crude-oil-wti": "WTI",
+    "crude-oil-brent": "BRENT",
+    "natural-gas": "NATURAL_GAS",
+    "copper": "COPPER",
+    "aluminium": "ALUMINIUM",
+    "wheat": "WHEAT",
+    "sugar": "SUGAR",
+    "cotton": "COTTON",
+    "corn": "CORN",
+    "coffee": "COFFEE",
+    "all": "ALL_COMMODITIES"
+};
 
-    const validInterval = ["daily", "weekly", "monthly"]
+// We create a dymanic route for commodities
+router.get("/price/:commodity/:interval", async (req, res) => {
+    const { commodity, interval } = req.params;
+
+    // We need to validate the interval
     if (!validInterval.includes(interval)) {
-        return res.status(500).json({ error: "Invalid input, please use daily, weekly or monthly" })
+        return res.status(400).json({ error: "Invalid interval. Please use daily, weekly, or monthly." });
+    }
+
+    // We need to also validate the commodity selected
+    const commodityFunction = validCommodities[commodity];
+    if (!commodityFunction) {
+        return res.status(400).json({ error: "Invalid commodity. Please provide a valid commodity type." });
     }
 
     try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=WTI&interval=${interval}&apikey=${apiKey}`)
+        // In here we make the fetch request with dynamic parameters such as commodityFunction, interval and apiKey
+        const response = await fetch(`https://www.alphavantage.co/query?function=${commodityFunction}&interval=${interval}&apikey=${apiKey}`);
         const data = await response.json();
-        res.json(data)
+
+        // Send the data to the client
+        res.json(data);
     } catch (error) {
-        res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
-    }
-});
-
-router.get("/price/crude-oil-brent/:interval", async (req, res) => {
-    const interval = req.params.interval
-
-    const validInterval = ["daily", "weekly", "monthly"]
-    if (!validInterval.includes(interval)) {
-        return res.status(500).json({ error: "Invalid input, please use daily, weekly or monthly" })
-    }
-
-    try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=BRENT&interval=${interval}&apikey=${apiKey}`)
-        const data = await response.json();
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
-    }
-});
-
-router.get("/price/natural-gas/:interval", async (req, res) => {
-    const interval = req.params.interval
-
-    const validInterval = ["daily", "weekly", "monthly"]
-    if (!validInterval.includes(interval)) {
-        return res.status(500).json({ error: "Invalid input, please use daily, weekly or monthly" })
-    }
-
-    try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=NATURAL_GAS&interval=${interval}&apikey=${apiKey}`)
-        const data = await response.json();
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
-    }
-});
-
-router.get("/price/copper/:interval", async (req, res) => {
-    const interval = req.params.interval
-
-    const validInterval = ["daily", "weekly", "monthly"]
-    if (!validInterval.includes(interval)) {
-        return res.status(500).json({ error: "Invalid input, please use daily, weekly or monthly" })
-    }
-
-    try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=COPPER&interval=${interval}&apikey=${apiKey}`)
-        const data = await response.json();
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
-    }
-});
-
-router.get("/price/aluminium/:interval", async (req, res) => {
-    const interval = req.params.interval
-
-    const validInterval = ["daily", "weekly", "monthly"]
-    if (!validInterval.includes(interval)) {
-        return res.status(500).json({ error: "Invalid input, please use daily, weekly or monthly" })
-    }
-
-    try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=ALUMINIUM&interval=${interval}&apikey=${apiKey}`)
-        const data = await response.json();
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
-    }
-});
-
-router.get("/price/wheat/:interval", async (req, res) => {
-    const interval = req.params.interval
-
-    const validInterval = ["daily", "weekly", "monthly"]
-    if (!validInterval.includes(interval)) {
-        return res.status(500).json({ error: "Invalid input, please use daily, weekly or monthly" })
-    }
-
-    try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=WHEAT&interval=${interval}&apikey=${apiKey}`)
-        const data = await response.json();
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
-    }
-});
-
-router.get("/price/sugar/:interval", async (req, res) => {
-    const interval = req.params.interval
-
-    const validInterval = ["daily", "weekly", "monthly"]
-    if (!validInterval.includes(interval)) {
-        return res.status(500).json({ error: "Invalid input, please use daily, weekly or monthly" })
-    }
-
-    try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=SUGAR&interval=${interval}&apikey=${apiKey}`)
-        const data = await response.json();
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
-    }
-});
-
-router.get("/price/cotton/:interval", async (req, res) => {
-    const interval = req.params.interval
-
-    const validInterval = ["daily", "weekly", "monthly"]
-    if (!validInterval.includes(interval)) {
-        return res.status(500).json({ error: "Invalid input, please use daily, weekly or monthly" })
-    }
-
-    try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=COTTON&interval=${interval}&apikey=${apiKey}`)
-        const data = await response.json();
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
-    }
-});
-
-router.get("/price/corn/:interval", async (req, res) => {
-    const interval = req.params.interval
-
-    const validInterval = ["daily", "weekly", "monthly"]
-    if (!validInterval.includes(interval)) {
-        return res.status(500).json({ error: "Invalid input, please use daily, weekly or monthly" })
-    }
-
-    try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=CORN&interval=${interval}&apikey=${apiKey}`)
-        const data = await response.json();
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
-    }
-});
-
-router.get("/price/coffee/:interval", async (req, res) => {
-    const interval = req.params.interval
-
-    const validInterval = ["daily", "weekly", "monthly"]
-    if (!validInterval.includes(interval)) {
-        return res.status(500).json({ error: "Invalid input, please use daily, weekly or monthly" })
-    }
-
-    try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=COFFEE&interval=${interval}&apikey=${apiKey}`)
-        const data = await response.json();
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
-    }
-});
-
-router.get("/price/all/:interval", async (req, res) => {
-    const interval = req.params.interval
-
-    const validInterval = ["daily", "weekly", "monthly"]
-    if (!validInterval.includes(interval)) {
-        return res.status(500).json({ error: "Invalid input, please use daily, weekly or monthly" })
-    }
-
-    try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=ALL_COMMODITIES&interval=${interval}&apikey=${apiKey}`)
-        const data = await response.json();
-        res.json(data)
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching data from Alpha Vantage" })
+        res.status(500).json({ error: "Error fetching data from Alpha Vantage." });
     }
 });
 
--- routes/technicalndicators.routes.js
+++ routes/technicalndicators.routes.js
@@ -4,19 +4,24 @@ const apiKey = 'UKYI48QGI96XRW9G';
 router.get(("/:symbol/:interval/:seriestype"), async (req, res) => {
     const interval = req.params.interval;
     const symbol = req.params.symbol;
-    const seriestype = req.params.seriestype;
+    const seriesType = req.params.seriestype;
+    const timePeriod = req.params.timePeriod;
 
     const validInterval = ["1min", "5min", "15min", "30min", "60min", "daily", "weekly", "monthly"]
     if (!validInterval.includes(interval)) {
         res.status(500).json({ error: "Input not valid" })
     }
     const validSeriesType = ["close", "open", "high", "low"]
-    if (!validSeriesType.includes(seriestype)) {
-        res.status(500).json({ error: "Input not valid" })
+    if (!validSeriesType.includes(seriesType)) {
+        res.status(500).json({ error: "Input not valid, use close, open, high, low" })
+    }
+
+    if (isNaN(timePeriod) || timePeriod <= 0) {
+        return res.status(500).json({ error: "Invalid time period, use 1min, 5min, 15min, 30min, 60min, daily, weekly, monthly" })
     }
 
     try {
-        const response = await fetch(`https://www.alphavantage.co/query?function=SMA&symbol=${symbol}&interval=${interval}&series_type=${seriestype}&apikey=${apiKey}`)
+        const response = await fetch(`https://www.alphavantage.co/query?function=SMA&symbol=${symbol}&interval=${interval}&series_type=${seriesType}&apikey=${apiKey}`)
         const data = await response.json()
         res.json(data)
     } catch (error) {
